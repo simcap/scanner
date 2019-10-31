ad5d8747fb9ca69d530fef7e5eb2e9707a5e4455
simcap <simcap@fastmail.com>
2019-10-31 15:42:43 +0100

    process more xml; improvments; print on console

--- scanner.py
+++ scanner.py
@@ -2,28 +2,48 @@ import os, sys, subprocess, argparse, tempfile
 import xml.etree.ElementTree as XML
 
 class NmapScan:
+
+    bin_name = 'nmap'
+
     def run(self, ips):
         xmlfile = tempfile.NamedTemporaryFile(suffix='-scan.xml', delete=False)
         process = subprocess.Popen(
-            ["nmap", "-F", "-Pn", "-n", "--stats-every", "2", "--open", "-oX", xmlfile.name, ] + ips,
+            ["nmap", "--stats-every", "1", "-oX", xmlfile.name, "-F", "-Pn", "-n"] + ips,
             stdout=subprocess.PIPE)
        
-        print 'scanning', ips
         while process.returncode is None:
             line = process.stdout.readline()
             if '% done' in line:
                 print(line.rstrip())
             process.poll()
+
+        process.wait()
         
-        print xmlfile.name
         results = Results(xmlfile.name)
         results.parse()
-        results.out()
+        results.write()
+
+        os.remove(xmlfile.name)
+
+    def verify_system(self):
+        discard = open(os.devnull, 'w')
+        if subprocess.call(['which', NmapScan.bin_name], stdout=discard, stderr=discard):
+            sys.exit('cannot find local {n} executable. {n} is required to run this program'.format(n=NmapScan.bin_name))
+        discard.close()
 
 class Host:
     def __init__(self, addr):
         self.addr = addr
-        self.open_ports = []
+        self.ports = []
+
+    def add_port(self, port):
+        self.ports.append(port)
+
+class Port:
+    def __init__(self, num, status, service):
+        self.num = num
+        self.status = status
+        self.service = service
 
 class Results:
     def __init__(self, filename):
@@ -33,23 +53,28 @@ class Results:
     def parse(self):
         tree = XML.parse(self.filename)
         root = tree.getroot()
-        self.hosts = []
-        for host in root.iter('host'):
-            address = host.find('address')
-            self.hosts.append(Host(address.get('addr')))
-
-    def out(self):
+        for host_element in root.findall('host'):
+            addr_element = host_element.find('address')
+            host =  Host(addr_element.get('addr'))
+            self.hosts.append(host)
+            for port_element in host_element.findall('ports/port'):
+                num = port_element.get('portid')
+                status = port_element.find('state').get('state')
+                service = port_element.find('service').get('name')
+                host.add_port(Port(num, status, service))    
+            
+    def write(self):
+        print '\n'
         for host in self.hosts:
             print host.addr
+            for port in host.ports:
+                print '\t', port.num, port.status, port.service
 
 parser = argparse.ArgumentParser(description='TCP Scan list of given IP addresses', epilog='Made by Simon with fun and love!')
 parser.add_argument('ips', help='IPv4 address to be scanned', nargs='*')
 args = parser.parse_args()
 
-if __name__ == '__main__':
-    discard = open(os.devnull, 'w')
-    if subprocess.call(["which", "nmap"], stdout=discard, stderr=discard):
-        sys.exit("cannot find local nmap executable. nmap is required to run this program")
-    
+if __name__ == '__main__':    
     scan = NmapScan()
+    scan.verify_system()
     scan.run(args.ips)
